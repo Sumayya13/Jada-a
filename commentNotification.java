69d16fea6b67d21a1597a094f62114cbe278b38a
shareeda <60987625+shareeda@users.noreply.github.com>
2020-11-09 05:44:09 +0300

    Add files via upload

--- commentNotification.java
+++ commentNotification.java
@@ -1,4 +1,5 @@
 package com.example.jadaa;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -11,16 +12,34 @@ import java.net.URL;
 import java.util.Scanner;
 import java.io.OutputStream;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.onesignal.OneSignal;
 public class commentNotification extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private String email, name, uid, uid_receiver,email_receiver,pId_re;
+    private DatabaseReference userDbRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.comment_notification);
         Button notification;
         notification = (Button) findViewById(R.id.notification);
+
 
         // Logging set to help debug issues, remove before releasing your app.
         OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
@@ -34,11 +53,37 @@ public class commentNotification extends AppCompatActivity {
         notification.setOnClickListener(new View.OnClickListener() {
                                             @Override
                                             public void onClick(View v) {
+
                                                 sendNotification();
 
                                             }
                                         }
         );
+        // change
+        mAuth = FirebaseAuth.getInstance();
+        checkUserStatus();
+        //get some user info to include in the post
+        userDbRef = FirebaseDatabase.getInstance().getReference("users");
+        Query query = userDbRef.orderByChild("email").equalTo(email);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    name = ""+ ds.child("fullName").getValue();
+                    email = ""+ ds.child("email").getValue();
+                    //user image goes here
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //=====change
     }
 
     //====
@@ -57,11 +102,65 @@ public class commentNotification extends AppCompatActivity {
                     String send_email;
 
                     //This is a Simple Logic to Send Notification different Device Programmatically....
-                    if (MainActivity.email.equals("user1@gmail.com")) {
-                        send_email = "user2@gmail.com";
-                    } else {
-                        send_email = "user1@gmail.com";
-                    }
+                    //
+
+
+                    firebaseAuth = FirebaseAuth.getInstance();
+                    user = firebaseAuth.getCurrentUser();
+                    firebaseDatabase = FirebaseDatabase.getInstance();
+                    databaseReference = firebaseDatabase.getReference("Posts");
+
+
+                    Query query = databaseReference.orderByChild("pId").equalTo(pId_re);// come from abeer
+                    query.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                //get Data
+                                uid_receiver = "" + ds.child("uid").getValue();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+//second
+                    firebaseAuth = FirebaseAuth.getInstance();
+                    user = firebaseAuth.getCurrentUser();
+                    firebaseDatabase = FirebaseDatabase.getInstance();
+                    databaseReference = firebaseDatabase.getReference("users");
+
+                    query = databaseReference.child(uid_receiver);
+                    query.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                //get Data
+                                email_receiver = "" + ds.child("email").getValue();
+
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                    //
+
+
+
+
+                    //
+                   // if (email.equals(mAuth.getCurrentUser().getEmail()))
+
+
 
                     try {
                         String jsonResponse;
@@ -79,7 +178,7 @@ public class commentNotification extends AppCompatActivity {
                         String strJsonBody = "{"
                                 + "\"app_id\": \"0523d5af-d75a-4916-a8dd-3e9109e0f10b\","
 
-                                + "\"filters\": [{\"field\": \"tag\", \"key\": \"User_ID\", \"relation\": \"=\", \"value\": \"" + send_email + "\"}],"
+                                + "\"filters\": [{\"field\": \"tag\", \"key\": \"User_ID\", \"relation\": \"=\", \"value\": \"" + email_receiver + "\"}],"
 
                                 + "\"data\": {\"foo\": \"bar\"},"
                                 + "\"contents\": {\"en\": \"English Message\"}"
@@ -118,4 +217,23 @@ public class commentNotification extends AppCompatActivity {
 
         //====
     }
+    // User status
+    private void checkUserStatus(){
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null){
+            //user is signed in stay here
+            //set email of logged user
+            email = user.getEmail();
+            uid = user.getUid();
+            //mProfile.setText(user.getEmail());
+        }else{
+            //user not signed in go to main activity
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+
+
+
+
 }
